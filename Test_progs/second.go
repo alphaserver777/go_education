23cf27cbefcb2903b6f3f78df73808f1ee8ea808
Cyber_polygon <maksim.ilonov@yandex.ru>
2025-01-13 10:45:52 +0300

    Обновление 1.1

--- Test_progs/second.go
+++ Test_progs/second.go
@@ -3,19 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 10
-	var b float64 = 3.14
-	var c string = "Hello"
-	var d bool = true
-
-	fmt.Println("a:", a)
-	fmt.Println("b:", b)
-	fmt.Println("c:", c)
-	fmt.Println("d:", d)
-
-	sum := a + 5
-	fmt.Println("Sum:", sum)
-
-	isEqual := a == 10
-	fmt.Println("Is equal:", isEqual)
+	num := 10
+	// [ ]: Создайте указатель на num
+	// TODO:
+	// NOTE: Измените значение num через указатель на 20
+	// BUG:
+	// FIXME:
+	// COMMENT:
+	// HACK
+	fmt.Println("Значение num:", num) // Ожидаемый вывод: 20
 }
